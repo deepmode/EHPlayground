12bb03b3a19f920f8abe2958bffd02df6748eb93
Eric Ho <ericho@101MediaLabs-iMac-Ho-Ho.local>
2016-10-25 13:54:21 +0800

    no message

--- FBInterview.playground/Contents.swift
+++ FBInterview.playground/Contents.swift
@@ -3,12 +3,38 @@
 import UIKit
 
 var str = "Hello, playground"
+enum MyType {
+    case numberType(x:Int)
+    case arrayType(x:[Int])
+}
 
-let inputValue:[Any] = [1,[4,3],6,[5,[1,0]]]
-inputValue
+let inputValue:[MyType] = [1,[4,3],6,[5,[1,0]]]
+
+func next(input:[Any]) -> [Int] {
+    var result = [Int]()
+    for each in input {
+        result.appendContentsOf(helper(each))
+    }
+    return result
+}
+
+func helper(element:Any) -> [Int] {
+    if let value = element as? Int {
+        return [value]
+    }
+    
+    
+    if let elementArray = element a Array {
+        
+    }
+//    for eachElement in element where eachElement as? Array {
+//        var result = [Int]()
+//        return result.appendContentsOf(helper(eachElement))
+//    }
+}
 
 
-
+/*
 func next(input:[Any]) ->[Int] {
     var inputArray = input
     if inputArray.count == 0 {
@@ -19,9 +45,11 @@ func next(input:[Any]) ->[Int] {
         return [eachElement]
     }
     inputArray.removeFirst()
-    
-    return NSMutableArray().addingObjects(from: next(input: inputArray)) as! [Int]
+    return NSMutableArray().addObjectsFromArray(next(inputArray))
+    //return NSMutableArray().addingObjects(from: next(input: inputArray)) as! [Int]
 }
+*/
+
 
 let c = next(input: inputValue)
 c.count
