588616dab810ac6910ce0ff68cbba5e3f0dcf3c9
Eric Ho <ericho@101MediaLabs-iMac-Ho-Ho.local>
2016-10-25 16:26:53 +0800

    no message

--- FBInterview.playground/Contents.swift
+++ FBInterview.playground/Contents.swift
@@ -3,55 +3,48 @@
 import UIKit
 
 var str = "Hello, playground"
-enum MyType {
-    case numberType(x:Int)
-    case arrayType(x:[Int])
-}
+//enum MyType {
+//    case numberType(x:Int)
+//    case arrayType(x:[Int])
+//}
 
-let inputValue:[MyType] = [1,[4,3],6,[5,[1,0]]]
+var result = [Any]()
+let a = [Any]() + [123]
+a
+
+let inputValue:[Any] = [1,[1,[2,3],["hello"]],[4,3],6,[5,[1,0]]]
 
 func next(input:[Any]) -> [Int] {
-    var result = [Int]()
-    for each in input {
-        result.appendContentsOf(helper(each))
-    }
-    return result
-}
-
-func helper(element:Any) -> [Int] {
-    if let value = element as? Int {
-        return [value]
-    }
     
-    
-    if let elementArray = element a Array {
-        
-    }
-//    for eachElement in element where eachElement as? Array {
-//        var result = [Int]()
-//        return result.appendContentsOf(helper(eachElement))
-//    }
-}
-
-
-/*
-func next(input:[Any]) ->[Int] {
-    var inputArray = input
-    if inputArray.count == 0 {
+    if input.count == 0 {
         return [Int]()
     }
     
-    if let eachElement = inputArray.first as? Int {
-        return [eachElement]
+    if let firstElement = input.first as? Int {
+        var remainder = input
+        remainder.removeFirst()
+        return [firstElement] + next(input: remainder)
     }
-    inputArray.removeFirst()
-    return NSMutableArray().addObjectsFromArray(next(inputArray))
-    //return NSMutableArray().addingObjects(from: next(input: inputArray)) as! [Int]
+    
+    if let firstArray = input.first as? [Any] {
+        var remainder = input
+        remainder.removeFirst()
+        return next(input: firstArray) + next(input: remainder)
+    } else {
+        var remainder = input
+        remainder.removeFirst()
+        return next(input: remainder)
+    }
 }
-*/
+
+
 
 
 let c = next(input: inputValue)
+let sortList = c.sorted{ (value1, value2) -> Bool in
+    value1 < value2
+}
+sortList
 c.count
 
 
